1ed670ac2c39dba4b85ba68ec44479cd7be47a13
Shantanu02-create <you@example.com>
2026-03-12 11:00:42 +0530

    updated code

--- day2.py
+++ day2.py
@@ -53,18 +53,45 @@
 
 # WAP to accept percentage and if per 
 # per>90==>grade A
-# per>80 and per<=90==>grade B
-# per>70 and per<=80==>grade C
-# per>60 and per<=70==>grade D  
+# per>80 and per<=90 ==>grade B
+# per>70 and per<=80 ==>grade C
+# per>60 and per<=70 ==>grade D  
 # per<=60==>FAIL
-per=float(input("Enter percentage: "))
-if per>90:
-    print("Grade A")
-elif per>80 and per<=90:
-    print("Grade B")
-elif per>70 and per<=80:
-    print("Grade C")
-elif per>60 and per<=70:
-    print("Grade D")
-else:
-    print("FAIL")
+# per=float(input("Enter percentage: "))
+# if per>90:
+#     print("Grade A")
+# elif per>80 and per<=90:
+#     print("Grade B")
+# elif per>70 and per<=80:
+#     print("Grade C")
+# elif per>60 and per<=70:
+#     print("Grade D")
+# else:
+#     print("FAIL")
+
+# mydict={"Name":"Shantanu","Age":20,"Sid":4849}
+# print(mydict)
+# print(type(mydict))
+# print(id(mydict))
+
+myd={101:"Shantanu",102:"Satyarth",'102':"Satyam",104:"Shivam",104:"Shivaji",106:"Adam"}
+# print(myd)
+# # a=myd[102]
+# # print(a)
+myd[104]="Prem"
+# print(myd)
+# for x in myd:
+#     print(x)
+# for y in myd.values():
+#     print(y)
+# items() method returns a view object that displays a list of dictionary's (key, value) tuple pairs.
+
+# for x,y in myd.items():  
+#     print(x," ",y)
+
+myd["Mobile"]=1234567890
+# print(myd)
+
+myd.pop(106)
+print(myd)
+
