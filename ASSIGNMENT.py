3bfeab5216c80e59e9d735df3e959f5f179e33bd
Shantanu02-create <you@example.com>
2026-03-16 10:08:13 +0530

    Initial commit

---
+++ ASSIGNMENT.py
@@ -0,0 +1,23 @@
+# # write a program to check two strings are anagrams or not using loop:
+# s1=input("Enter first string: ")
+# s2=input("Enter second string: ")
+# s11=s1.split()
+# s22=s2.split()
+# if (len(s11)!=len(s22)) or s11.sort()!=s22.sort():
+#     print("The strings are not anagrams.")
+# else:
+#     print("The strings are anagrams.")
+
+# write a program to chaech if given string is palindrome or not using loop:
+# s=input("Enter a string: ")
+# reversed_s=""
+# for i in range(len(s)-1,-1,-1):
+#     reversed_s+=s[i]
+# if s==reversed_s:
+#     print("The string is a palindrome.")
+#     print("Original string: ",s)
+#     print("Reversed string: ",reversed_s)
+# else:    
+#     print("The string is not a palindrome.")  
+#     print("Original string: ",s)
+#     print("Reversed string: ",reversed_s)
